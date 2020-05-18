fe3b6142e3cf3a21f9ff3e2a7dacbb7c29e73005
mari <mari.17ymy@gmail.com>
2020-05-17 22:33:08 -0400

    bottomNavigationBar

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,14 +1,45 @@
 
 import 'package:flutter/material.dart';
+import 'package:qr_react_app/src/pages/direcciones_page.dart';
+import 'package:qr_react_app/src/pages/mapas_page.dart';
  
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+
+  int currentI = 0;
 
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('hola home page'),
-      ),
+      body: _callPage(currentI),
+      bottomNavigationBar: _crearBottonNavigationBar(),
+    );
+  }
+
+  Widget _callPage(int paginaActual){
+    switch (paginaActual) {
+      case 0: return DireccionesPage();
+      case 1: return MapasPage();
+      default: return DireccionesPage();
+    }
+  }
+
+  Widget _crearBottonNavigationBar(){
+    return BottomNavigationBar(
+      currentIndex: currentI,
+      onTap: (index){
+        setState(() {
+          currentI = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(icon: Icon(Icons.ac_unit), title: Text('direcciones')),
+        BottomNavigationBarItem(icon: Icon(Icons.map), title: Text('mapas')),
+      ]
     );
   }
 }
